e5eb4e5a5f69bb441c0a63382597d1367b437e48
blossomvn1 <55186741+blossomvn1@users.noreply.github.com>
2020-12-27 21:43:34 +0700

    update Transaction.sql

--- Transaction.sql
+++ Transaction.sql
@@ -1,10 +1,4 @@
-﻿UPDATE dbo.Account 
-SET id = 2 WHERE UserName='staff'
-
-SELECT * FROM dbo.Food
-
-SELECT * FROM dbo.SANPHAM
-
+﻿
 CREATE TABLE account
 (
 	id INT,
@@ -44,7 +38,7 @@ VALUES
 	UPDATE dbo.account SET displayname = 'test'
 
 	SELECT * FROM dbo.account
-	CREATE PROC USP_GetAccountByUserName
+CREATE PROC USP_GetAccountByUserName
 @userName nvarchar(100)
 AS 
 BEGIN
@@ -61,46 +55,8 @@ BEGIN
 END 
 GO
 
-SELECT * FROM dbo.SANPHAM
 
-INSERT INTO dbo.SANPHAM
-(
-    IDSP,
-    IDDANHMUC,
-    TENSP,
-    MOTASP,
-    ANHSP,
-    VIDEOSP,
-    THUONGHIEU,
-    GIASP,
-    MASKU,
-    TINHTRANG_
-)
-VALUES
-(   0,    -- IDSP - int
-    0,    -- IDDANHMUC - int
-    '',   -- TENSP - varchar(100)
-    '',   -- MOTASP - varchar(3000)
-    '',   -- ANHSP - varchar(50)
-    '',   -- VIDEOSP - varchar(50)
-    '',   -- THUONGHIEU - varchar(50)
-    NULL, -- GIASP - money
-    '',   -- MASKU - varchar(20)
-    0     -- TINHTRANG_ - tinyint
-    
-
-	SELECT * FROM dbo.CHITIETDM
-	SELECT * FROM dbo.DANHMUC
-
-
-	SELECT DISTINCT * FROM (dbo.SANPHAM JOIN dbo.SANPHAMKB ON SANPHAMKB.IDSP = SANPHAM.IDSP) JOIN dbo.KHACHBAN KB ON KB.IDKH= dbo.SANPHAMKB.IDKH  WHERE dbo.SANPHAMKB.IDKH='1'
-	SELECT * FROM dbo.KHACHBAN
-
-	SELECT * FROM dbo.KHACHHANG
-	SELECT * FROM dbo.DONMUA
-	
-
-	CREATE PROC USP_InsertBillInfo
+CREATE PROC USP_InsertBillInfo
 @idBill INT, @idProduct INT, @count INT
 AS
 BEGIN
@@ -137,6 +93,7 @@ BEGIN
 END
 GO
 
+
 CREATE PROC USP_InsertBill
 @idCus INT
 AS
@@ -168,12 +125,7 @@ VALUES
 			  0 
 	        )*/
 END
-SELECT * FROM dbo.CHITIETDM
-SELECT * FROM dbo.DONMUA WHERE IDKH =1
-SELECT * FROM dbo.KHACHHANG
-SELECT * FROM dbo.KHACHBAN
-SELECT * FROM dbo.KHACHMUA
-SELECT * FROM dbo.KHO
+
 
 CREATE FUNCTION [dbo].[GetUnsignString](@strInput NVARCHAR(4000)) 
 RETURNS NVARCHAR(4000)
@@ -213,7 +165,9 @@ BEGIN
     END
     RETURN @strInput
 END
-ALTER PROCEDURE USP_InsertProduct @iddm INT, @tensp VARCHAR(50),@motasp VARCHAR(100),@anh VARCHAR(50),@video VARCHAR(50),@thuonghieu VARCHAR(10),@gia FLOAT,@maSKU VARCHAR(10),@tinhtrang TINYINT
+
+-----------------
+CREATE PROCEDURE USP_InsertProduct @iddm INT, @tensp VARCHAR(50),@motasp VARCHAR(100),@anh VARCHAR(50),@video VARCHAR(50),@thuonghieu VARCHAR(10),@gia FLOAT,@maSKU VARCHAR(10),@tinhtrang TINYINT
 as
 BEGIN
 DECLARE @num INT
@@ -246,7 +200,9 @@ VALUES
 END
 
 EXEC USP_InsertProduct 1, 'abc', 'abc' ,'1' ,'1' ,'abc' ,1000,'123' ,1
-ALTER PROC usp_deleteproduct @idsp int
+
+
+CREATE PROC usp_deleteproduct @idsp int
 AS
 BEGIN
     alter table dbo.CHITIETDG nocheck constraint ALL
@@ -264,8 +220,9 @@ BEGIN
     ALTER table dbo.DANHGIASP check constraint ALL
     ALTER table dbo.SANPHAMKB check constraint all
 END
-SELECT * FROM dbo.KHO
-ALTER PROC usp_insertInventory @idnv INT,@tenkho VARCHAR (50),@diachi VARCHAR(100)
+
+
+CREATE PROC usp_insertInventory @idnv INT,@tenkho VARCHAR (50),@diachi VARCHAR(100)
 AS
 DECLARE @numInventory INT
 SET @numInventory = (SELECT MAX(IDKHO) FROM dbo.KHO) +1
@@ -283,11 +240,21 @@ VALUES
     @diachi  -- DIACHIKHO - varchar(100)
     )
 
-	UPDATE dbo.KHO 
-	SET TENKHO ='Kho C'
-	WHERE IDKHO=2
-
-ALTER PROC usp_deleteInventory @idkho int
+	
+	------
+CREATE PROC sp_capnhatKho @idInven INT, @ten VARCHAR(50),@diachi VARCHAR(100),@idnv int 
+AS 
+BEGIN 
+		
+	UPDATE dbo.KHO
+	SET TENKHO=@ten,
+		IDNV=@idnv,
+		DIACHIKHO=@diachi
+		WHERE IDKHO=@idInven
+    END 
+	EXEC sp_capnhatKho 100,'Kho ABC','123',4
+	--------------------
+CREATE PROC usp_deleteInventory @idkho int
 AS
 BEGIN
      alter table dbo.CHITIETKHO NOCHECK constraint ALL
@@ -298,14 +265,15 @@ BEGIN
 END
 EXEC usp_deleteInventory 481
 
-SELECT * FROM dbo.KHACHHANG
 
+--------
 SELECT dbo.DONGIAO.IDDG, SOLUONGGIAO*SP.GIASP AS TONGTIEN FROM (dbo.DONGIAO JOIN dbo.CHITIETDG ON CHITIETDG.IDDG = DONGIAO.IDDG) JOIN dbo.SANPHAM SP ON SP.IDSP = dbo.CHITIETDG.IDSP WHERE TTGIAO = 1
 CREATE PROC sp_timSPKB @idCus INT
 AS
 BEGIN
     SELECT * FROM dbo.SANPHAM JOIN dbo.SANPHAMKB ON SANPHAMKB.IDSP = SANPHAM.IDSP WHERE IDKH=@idCus
 END
+---------
 CREATE PROC sp_capnhatKho @idKHO INT,@idNV INT,@tenKHO VARCHAR(50),@diachi VARCHAR(100)
 AS
 BEGIN
@@ -318,3 +286,210 @@ BEGIN
 			IDNV=@idNV
 		WHERE IDKHO=idKHO
 END
+
+
+CREATE PROC sp_donmua @idkh INT,@ngaymua DATE
+AS
+BEGIN
+	DECLARE @iddm INT
+    SET @iddm = (SELECT MAX(IDDM) FROM dbo.DONMUA)+1
+	INSERT dbo.DONMUA (IDDM,IDKH,NGAYMUA,TTXEM)VALUES(@iddm,@idkh,GETDATE(),0)
+END
+CREATE PROC sp_chitietdonmua @iddm INT,@idsp INT,@sl INT
+AS
+BEGIN
+    INSERT dbo.CHITIETDM(IDDM,IDSP,SOLUONGMUA)
+    VALUES (@iddm, @idsp, @sl)
+END
+
+
+CREATE PROC sp_thongkeHT_KB @idkh int
+AS
+BEGIN
+    SELECT SP.IDSP,SP.TENSP,SPKB.SOLUONGHT FROM dbo.SANPHAM SP JOIN dbo.SANPHAMKB SPKB ON SPKB.IDSP = SP.IDSP WHERE SPKB.IDKH=@idkh
+END
+EXEC sp_thongkeHT_KB 3
+CREATE CLUSTERED INDEX index_sanpham ON dbo.SANPHAM (IDSP)
+DROP INDEX index_sanpham ON dbo.SANPHAM
+------------------ Giao tac 2---------------------
+
+create PROC sp_xoaKH @idkh INT
+AS
+BEGIN
+  alter table dbo.DIACHIKH NOCHECK constraint ALL
+  alter table dbo.KHACHBAN NOCHECK constraint ALL
+  ALTER TABLE dbo.KHACHMUA NOCHECK CONSTRAINT ALL 
+  ALTER TABLE dbo.SANPHAMKB NOCHECK CONSTRAINT ALL 
+  ALTER TABLE dbo.DANHGIASP NOCHECK CONSTRAINT ALL
+  ALTER TABLE dbo.DONGIAO NOCHECK CONSTRAINT ALL
+  ALTER TABLE dbo.DONMUA NOCHECK CONSTRAINT ALL
+  ALTER TABLE dbo.MAGIAMKM NOCHECK CONSTRAINT ALL
+	IF(EXISTS(SELECT * FROM dbo.KHACHMUA WHERE IDKH=@idkh) AND EXISTS(SELECT * FROM dbo.KHACHBAN WHERE IDKH=@idkh))
+		DELETE dbo.KHACHMUA WHERE IDKH=@idkh
+		DELETE dbo.KHACHHANG WHERE IDKH=@idkh
+		DELETE dbo.DIACHIKH WHERE IDKH=@idkh
+		DELETE dbo.KHACHBAN WHERE IDKH=@idkh
+		DELETE dbo.SANPHAMKB WHERE IDKH=@idkh
+		DELETE dbo.MAGIAMKM WHERE IDKH=@idkh
+	IF(EXISTS(SELECT * FROM dbo.KHACHMUA WHERE IDKH=@idkh) AND NOT EXISTS(SELECT * FROM dbo.KHACHBAN WHERE IDKH=@idkh))
+		DELETE dbo.KHACHMUA WHERE IDKH=@idkh
+		DELETE dbo.KHACHHANG WHERE IDKH=@idkh
+		DELETE dbo.DIACHIKH WHERE IDKH=@idkh
+		DELETE dbo.MAGIAMKM WHERE IDKH=@idkh
+	IF(NOT EXISTS(SELECT * FROM dbo.KHACHMUA WHERE IDKH=@idkh) AND EXISTS(SELECT * FROM dbo.KHACHBAN WHERE IDKH=@idkh))
+		DELETE dbo.KHACHHANG WHERE IDKH=@idkh
+		DELETE dbo.DIACHIKH WHERE IDKH=@idkh
+		DELETE dbo.KHACHBAN WHERE IDKH=@idkh
+		DELETE dbo.SANPHAMKB WHERE IDKH=@idkh
+	ALTER TABLE dbo.MAGIAMKM CHECK CONSTRAINT all
+	ALTER TABLE dbo.DONMUA CHECK CONSTRAINT all
+	ALTER TABLE dbo.DONGIAO CHECK CONSTRAINT all
+	ALTER table dbo.DIACHIKH check constraint ALL
+	ALTER table dbo.KHACHBAN check constraint ALL
+	ALTER TABLE dbo.KHACHMUA CHECK CONSTRAINT ALL
+    ALTER TABLE dbo.SANPHAMKB CHECK CONSTRAINT ALL
+    ALTER TABLE dbo.DANHGIASP CHECK CONSTRAINT all
+END
+EXEC sp_xoaKH  125
+
+------------------ Giao tac 1---------------------
+create function f_timIDKH() 
+returns int 
+as 
+begin 
+	return (select max(IDKH) from KHACHHANG) 
+end 
+go 
+create proc sp_themKhachHang  
+	@ten varchar(40),  
+	@gioi varchar(3), 
+	@ngaysinh date, 
+	@email varchar(40), 
+	@dt char(10), 
+	@tk varchar(16) 
+as 
+begin tran 
+	insert KHACHHANG 
+	values(dbo.f_timIDKH(), @ten, @gioi, @ngaysinh, @email, @dt, @tk) 
+	if @@ERROR != 0 
+		rollback tran 
+commit tran 
+GO 
+dbo.sp_themKhachHang @ten = 'Nguyen Van B',                -- varchar(40)
+                     @gioi = 'Nam',               -- varchar(3)
+                     @ngaysinh = '2000-12-27', -- date
+                     @email = '123@gmail.com',              -- varchar(40)
+                     @dt = '123',                 -- char(10)
+                     @tk = '123'                  -- varchar(16)
+CREATE INDEX index_KhachHang ON dbo.KHACHHANG (IDKH)
+CREATE CLUSTERED INDEX index_SP ON dbo.SANPHAM (IDSP)
+DROP INDEX index_SP ON dbo.SANPHAM
+DROP INDEX index_sanpham ON dbo.SANPHAM 
+EXEC sp_thongkeHT_KB 100
+
+EXEC sp_xoaKH 150
+
+
+CREATE PROC sp_timSP @idsp int 
+AS 
+BEGIN 
+    SELECT * FROM SANPHAM where IDSP=@idsp 
+END 
+
+EXEC sp_timSP 499
+
+
+CREATE PROC sp_timthongtindonmuahang @id int,@ngaymua datetime
+AS
+BEGIN
+    SELECT * FROM dbo.DONMUA WHERE IDDM=@id AND NGAYMUA=@ngaymua
+END
+EXEC sp_timthongtindonmuahang 100,'2000-01-01'
+
+
+CREATE PROC sp_timKH 
+AS 
+BEGIN 
+    SELECT * FROM KHACHHANG 
+END 
+
+
+CREATE PROC sp_timKho @idkho int 
+AS 
+BEGIN 
+    SELECT * FROM KHO where IDKHO=@idkho 
+END 
+
+EXEC sp_timKH
+EXEC sp_timKho 400
+
+
+CREATE PROC sp_xoaKho @idKho int
+AS 
+BEGIN 
+    alter table dbo.CHITIETKHO NOCHECK constraint ALL 
+    alter table dbo.DONGIAO NOCHECK constraint ALL 
+    DELETE FROM kho WHERE IDKHO=@idKho 
+    ALTER table dbo.CHITIETKHO check constraint ALL 
+    ALTER table dbo.DONGIAO check constraint ALL 
+END 
+
+EXEC sp_xoaKho 450
+
+
+CREATE PROC sp_themKho @idnv INT,@tenkho VARCHAR (50),@diachi VARCHAR(100) 
+AS 
+BEGIN 
+    DECLARE @numInventory INT 
+    SET @numInventory = (SELECT MAX(IDKHO) FROM dbo.KHO) +1 
+    INSERT dbo.KHO 
+( 
+    IDKHO, 
+    IDNV, 
+    TENKHO, 
+    DIACHIKHO 
+) 
+VALUES 
+(   @numInventory,  -- IDKHO - int 
+    @idnv,  -- IDNV - int 
+    @tenkho, -- TENKHO - varchar(40) 
+    @diachi  -- DIACHIKHO - varchar(100) 
+    ) 
+END 
+EXEC sp_themKho 450, 'Kho B1','250 Tran Phu, Quan 7,TPHCM'
+
+
+CREATE PROC sp_capnhatKho @idInven INT, @ten varchar(50) ,@idnv int, @diachi varchar(100)
+AS 
+BEGIN 
+    UPDATE dbo.KHO 
+	SET TENKHO=@ten, 
+	IDNV=@idnv, 
+	DIACHIKHO=@diachi 
+	WHERE IDKHO=@idInven 
+    END 
+	
+	EXEC sp_capnhatKho 120,'Kho H5',59,'Nguyen Thai Hoc, Quan 7, TPHCM'
+
+
+CREATE PROC sp_timSPKB @idCus INT 
+AS 
+BEGIN 
+    SELECT * FROM dbo.SANPHAM JOIN dbo.SANPHAMKB ON SANPHAMKB.IDSP = SANPHAM.IDSP WHERE IDKH=@idCus
+END 
+EXEC sp_timSPKB 230
+CREATE PROC sp_timHienThiSP 
+AS 
+BEGIN 
+    SELECT * FROM SANPHAM 
+END 
+EXEC sp_timHienThiSP
+SELECT * FROM dbo.SANPHAM
+
+EXEC dbo.sp_themKhachHang @ten = 'Tran Thi B',                -- varchar(40)
+                          @gioi = 'Nu',               -- varchar(3)
+                          @ngaysinh = '2002-12-27', -- date
+                          @email = '456@gmail.com',              -- varchar(40)
+                          @dt = '09808',                 -- char(10)
+                          @tk = '123456'                  -- varchar(16)
+
